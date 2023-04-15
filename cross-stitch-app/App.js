bc991063d9349245d3b475231dec234d9166c430
Taylor.Payne <taylor.payne@uky.edu>
2023-04-15 03:14:14 -0400

    Added Image Frame and place holder box

--- cross-stitch-app/App.js
+++ cross-stitch-app/App.js
@@ -9,14 +9,34 @@ export default function App() {
   const [image, setImage] = useState(null) // image hook 
 
   const BackgroundImg = () => {
+  const currentImage = require('./assets/Greedent.png')
   return (
     <View>
+      {/* displaying background */}
       <ImageBackground
         source={
          require("./assets/back.png")
         }
         resizeMode="stretch"
-        style={styles.image}>
+        style={styles.backImage}>
+          {/* displaying box frame for image */}
+          <ImageBackground source={require("./assets/board.png")} 
+                  style={{ width: 300, 
+                           height: 300,
+                           marginBottom: 20,
+                           marginTop: 20,
+                           justifyContent: 'center',
+                           alignItems: 'center',
+          }}>
+          {/* displaying image selected or logo */}
+          <Image source={currentImage} 
+                 style ={{width: 100,
+                         height: 100,
+                         margin: 10
+                 }}/>
+          </ImageBackground>
+          
+
         <Button title="Pick an image from camera roll" onPress={pickImage} />
         {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
       </ImageBackground>
--- cross-stitch-app/styles/home.js
+++ cross-stitch-app/styles/home.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },
 
-    image: {
+    backImage: {
       height: screenHeight,
       width: screenWidth,
       justifyContent: 'center',
