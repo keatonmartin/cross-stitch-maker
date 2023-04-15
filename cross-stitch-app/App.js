281d06e3314a990d8c6aeddf43d5f816cca98afa
Taylor.Payne <taylor.payne@uky.edu>
2023-04-15 01:47:29 -0400

    Add background image

--- cross-stitch-app/App.js
+++ cross-stitch-app/App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Text, View, Button, Image } from 'react-native';
+import { Text, View, Button, Image, ImageBackground } from 'react-native';
 import { styles } from './styles/home';
 import * as ImagePicker from 'expo-image-picker'
+
 
 export default function App() {
   const [image, setImage] = useState(null) // image hook 
 
+  const BackgroundImg = () => {
+  return (
+    <View>
+      <ImageBackground
+        source={
+         require("./assets/back.png")
+        }
+        resizeMode="stretch"
+        style={styles.image}>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </ImageBackground>
+    </View>
+  );
+};
   const pickImage = async() => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -22,12 +38,11 @@ export default function App() {
     }
   }
 
+
   return (
     <View style={styles.container}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <BackgroundImg/>
     </View>
-
   );
 }
 
