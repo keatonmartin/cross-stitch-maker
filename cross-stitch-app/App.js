6fe8a3d9b44d126d9a7de4693a4df9e8c09dd35e
Keaton Martin <ktma234@uky.edu>
2023-04-14 20:43:35 -0400

    Add basic image picker functionality

--- cross-stitch-app/App.js
+++ cross-stitch-app/App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, Image } from 'react-native';
 import { styles } from './styles/home';
+import * as ImagePicker from 'expo-image-picker'
 
 export default function App() {
+  const [image, setImage] = useState(null) // image hook 
+
+  const pickImage = async() => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      // allowsEditing: true,
+      aspect: [4, 3],
+      // quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
     </View>
+
   );
 }
 
