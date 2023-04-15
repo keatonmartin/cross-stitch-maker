ecaf3bcda256c90861191c599179d7de11b16d7e
Taylor.Payne <taylor.payne@uky.edu>
2023-04-15 14:52:23 -0400

    Fixed the Glider to be a Background image

--- cross-stitch-app/App.js
+++ cross-stitch-app/App.js
@@ -33,8 +33,8 @@ export default function App() {
                             justifyContent: 'center',
                             alignItems: 'center',
             }}>
-              
-            <Image source={require('./assets/glider-green.png')} 
+
+            <ImageBackground source={require('./assets/glider-green.png')} 
                    style={{width: 125, 
                            height: 125 ,
                            alignItems: 'center'
