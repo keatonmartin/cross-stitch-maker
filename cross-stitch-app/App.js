dc350c77d9e295f055cfe3ec227ed88afa9c72f4
Keaton Martin <ktma234@uky.edu>
2023-04-15 04:16:26 -0400

    Implement basic image upload/download with API

--- cross-stitch-app/App.js
+++ cross-stitch-app/App.js
@@ -4,8 +4,11 @@ import { Text, View, Button, Image } from 'react-native';
 import { styles } from './styles/home';
 import * as ImagePicker from 'expo-image-picker'
 
+const ENDPOINT = 'https://us-central1-credible-rider-383823.cloudfunctions.net/ml'
+
 export default function App() {
   const [image, setImage] = useState(null) // image hook 
+  const [blobData, setBlobData] = useState(null)
 
   const pickImage = async() => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -15,17 +18,39 @@ export default function App() {
       // quality: 1,
     });
 
-    console.log(result);
-
     if (!result.cancelled) {
-      setImage(result.uri);
+      setImage(result);
     }
   }
+
+  useEffect(() => { // payload effect hook
+    if (image) { // if image has been loaded, send payload
+      var photo = { 
+        uri: image.uri,
+        type: "image/jpeg",
+        name: "photo.jpg",
+      }
+      var body = new FormData();
+      body.append('image', photo);
+      fetch(ENDPOINT, {
+        method: "POST",
+        body
+      }).then(res => res.blob())
+      .then((blob) => {
+        const fileReaderInstance = new FileReader();
+        fileReaderInstance.readAsDataURL(blob);
+        fileReaderInstance.onload = () => {
+          base64data = fileReaderInstance.result;
+          setBlobData(base64data)
+        }
+      })
+    }
+  }, [image])
 
   return (
     <View style={styles.container}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      {blobData && <Image source={{ uri: blobData }} style={{ width: 200, height: 200 }} />}
     </View>
 
   );
